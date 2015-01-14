a12e6ceb4755664b79ccf7170272bbba8125a49a
Antonio Caravaca Vega <tony@iMac24.local>
2015-01-14 19:07:10 +0100

    Completed Project

---
+++ SlotMachine/Slot.swift
@@ -0,0 +1,18 @@
+//
+//  Slot.swift
+//  SlotMachine
+//
+//  Created by Antonio Caravaca Vega on 18/12/14.
+//  Copyright (c) 2014 Antonio Caravaca Vega. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Slot {
+    
+    var value = 0
+    var image = UIImage(named: "Ace")
+    var isRed = true
+    
+}
